fe585a9a244db17b500765e1a8827f8487caf6e0
abhinavcreed13 <abhinavcs13@gmail.com>
2020-08-06 15:10:40 +1000

    updating scripts and outlines

--- spatial-qgis/scripts/data_attributes_cleaner.py
+++ spatial-qgis/scripts/data_attributes_cleaner.py
@@ -1,15 +1,14 @@
 from qgis.core import QgsProject
 
-layer = QgsProject.instance().mapLayersByName("PAR_BUILDING_OUTLINE")[0]
+layers = QgsProject.instance().mapLayers().values()
 
-iface.setActiveLayer(layer)
+for layer in layers:
+    layer_provider = layer.dataProvider()
 
-layer_provider = layer.dataProvider()
+    # clean MR_WEIGHTS
+    weightFieldIndex = layer_provider.fieldNameIndex('MR_WEIGHTS')
 
-# clean MR_WEIGHTS
-weightFieldIndex = layer_provider.fieldNameIndex('MR_WEIGHTS')
-
-if weightFieldIndex != -1:
-    layer_provider.deleteAttributes([weightFieldIndex])
-    layer.updateFields()
-    print("MR_WEIGHTS removed")
+    if weightFieldIndex != -1:
+        layer_provider.deleteAttributes([weightFieldIndex])
+        layer.updateFields()
+        print("MR_WEIGHTS removed")
