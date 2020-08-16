e12c32feef65720b0fb98a6044570b90968e8826
advait22 <deshpandeadvait88@gmail.com>
2020-08-16 16:08:59 +1000

    complete automation script for data enhancer

--- spatial-qgis/scripts/Data_Enhancer_auto.py
+++ spatial-qgis/scripts/Data_Enhancer_auto.py
@@ -10,6 +10,7 @@
 *                                                                         *
 ***************************************************************************
 """
+
 from qgis.PyQt.QtCore import QCoreApplication,QVariant
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
@@ -28,23 +29,8 @@ from qgis.core import (QgsProcessing,
 from qgis import processing
 import pandas as pd
 
-class CustomProcessingAlgorithm(QgsProcessingAlgorithm):
-    """
-    This is an example algorithm that takes a vector layer and
-    creates a new identical one.
 
-    It is meant to be used as an example of how to create your own
-    algorithms and explain methods and variables used to do it. An
-    algorithm like this will be available in all elements, and there
-    is not need for additional work.
-
-    All Processing algorithms should extend the QgsProcessingAlgorithm
-    class.
-    """
-
-    # Constants used to refer to parameters and outputs. They will be
-    # used when calling the algorithm from another algorithm, or when
-    # calling from the QGIS console.
+class DataEnhancer(QgsProcessingAlgorithm):
 
     INPUT = 'INPUT'
     OUTPUT = 'OUTPUT'
@@ -53,98 +39,52 @@ class CustomProcessingAlgorithm(QgsProcessingAlgorithm):
     SEARCH_KEY = "search_key"
 
     def tr(self, string):
-        """
-        Returns a translatable string with the self.tr() function.
-        """
         return QCoreApplication.translate('Processing', string)
 
     def createInstance(self):
-        return CustomProcessingAlgorithm()
+        return DataEnhancer()
 
     def name(self):
-        """
-        Returns the algorithm name, used for identifying the algorithm. This
-        string should be fixed for the algorithm, and must not be localised.
-        The name should be unique within each provider. Names should contain
-        lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
-        return 'Data Loader Script'
+        return 'Data Loader'
 
     def displayName(self):
-        """
-        Returns the translated algorithm name, which should be used for any
-        user-visible display of the algorithm name.
-        """
         return self.tr('Data Loader Script')
 
     def group(self):
-        """
-        Returns the name of the group this algorithm belongs to. This string
-        should be localised.
-        """
-        return self.tr('scripts')
+        return self.tr('Scripts')
 
     def groupId(self):
-        """
-        Returns the unique ID of the group this algorithm belongs to. This
-        string should be fixed for the algorithm, and must not be localised.
-        The group id should be unique within each provider. Group id should
-        contain lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
-        return 'scripts'
+        return 'Scripts'
 
     def shortHelpString(self):
-        """
-        Returns a localised short helper string for the algorithm. This string
-        should provide a basic description about what the algorithm does and the
-        parameters and outputs associated with it..
-        """
-        return self.tr("Creates custom layers according to the requirment")
+        return self.tr("Loading data into QGIS environment")
 
-    
     def initAlgorithm(self, config=None):
-        """
-        Here we define the inputs and output of the algorithm, along
-        with some other properties.
-        """
-
-        # We add the input vector features source. It can have any kind of
-        # geometry.
         self.addParameter(
             QgsProcessingParameterFeatureSource(
-            self.INPUT,
-            self.tr('Input layer'),
-            [QgsProcessing.TypeVectorAnyGeometry]
+                self.INPUT,
+                self.tr('Input layer'),
+                [QgsProcessing.TypeVectorAnyGeometry]
             )
         )
-        
         self.addParameter(
             QgsProcessingParameterString(
                 self.BASE_URL,
-                'Enter Base URL',
-                defaultValue="C:/Users/deshp/Google Drive/project-space-optimisation-group-3/project-data/"
+                'Enter Base URL'
             )
         )
         self.addParameter(
             QgsProcessingParameterString(
                 self.CAMPUS_CODE,
-                'Enter Campus Code',
-                defaultValue="par"
-
+                'Enter Campus Code'
             )
         )
         self.addParameter(
             QgsProcessingParameterString(
                 self.SEARCH_KEY,
-                'Enter Search Key',
-                defaultValue="BUILD_NO"
+                'Enter Search Key'
             )
         )
-        # We add a feature sink in which to store our processed features (this
-        # usually takes the form of a newly created vector layer when the
-        # algorithm is run in QGIS).
         self.addParameter(
             QgsProcessingParameterFeatureSink(
                 self.OUTPUT,
@@ -153,19 +93,11 @@ class CustomProcessingAlgorithm(QgsProcessingAlgorithm):
         )
 
     def processAlgorithm(self, parameters, context, feedback):
-        """
-        Here is where the processing itself takes place.
-        """
-
-        # Retrieve the feature source and sink. The 'dest_id' variable is used
-        # to uniquely identify the feature sink, and must be included in the
-        # dictionary returned by the processAlgorithm function.
         source = self.parameterAsSource(
             parameters,
             self.INPUT,
             context
         )
-        
         base_url = self.parameterAsString(
             parameters,
             self.BASE_URL,
@@ -187,41 +119,18 @@ class CustomProcessingAlgorithm(QgsProcessingAlgorithm):
         base_url = base_url.strip()
         campus_code = campus_code.strip().lower()
         search_key = search_key.strip()
-
-        fields = QgsFields()
-        fields.append(QgsField(base_url, QVariant.String))
-        fields.append(QgsField(campus_code,QVariant.String))
-        fields.append(QgsField(search_key,QVariant.String))
-        # fields.append(QgsField('MR_WEIGHTS',QVariant.Double))
-
-        # weightFieldIndex = source.indexFromName('MR_WEIGHTS')
-        # if weightFieldIndex != -1:
-        #     removeFields = fields.indexOf('MR_WEIGHTS')
-        #     fields.remove(removeFields)
-        # else:
-        #     fields.append(QgsField('MR_WEIGHTS',QVariant.Double))
-
-
-
-        # If source was not found, throw an exception to indicate that the algorithm
-        # encountered a fatal error. The exception text can be any string, but in this
-        # case we use the pre-built invalidSourceError method to return a standard
-        # helper text for when a source cannot be evaluated
-
-
-        if source is None:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
-
-        (sink, dest_id) = self.parameterAsSink(
-            parameters,
-            self.OUTPUT,
-            context,
-            fields,
-            source.wkbType(),
-            source.sourceCrs()
-        )
-
-        # Send some information to the user
+        data = parameters['INPUT'].split('_')
+        input_param = data[0]+"_"+data[1]+"_"+data[2]
+        layer = QgsProject.instance().mapLayersByName(input_param)[0]
+        features = layer.getFeatures()
+        layer_provider = layer.dataProvider()
+        weightFieldIndex = source.fields().indexFromName('MR_WEIGHTS')
+        if weightFieldIndex != -1:
+            layer_provider.deleteAttributes([weightFieldIndex])
+            layer.updateFields()
+        layer_provider.addAttributes([QgsField("MR_WEIGHTS",  QVariant.Double)])
+        layer.updateFields()  
+        weightFieldIndex = layer_provider.fieldNameIndex('MR_WEIGHTS')
         uom_space_url = base_url+'uom-space.xlsx'
         rm_category_type_url = base_url+'rm-category-type-cleaned.xlsx'
         em_location_url = base_url+'em-location.xlsx'
@@ -236,79 +145,66 @@ class CustomProcessingAlgorithm(QgsProcessingAlgorithm):
         _extractor = DataExtractor(feedback)
         possible_meeting_rooms_data = _extractor.get_meeting_rooms_data(_processor.rm_category_type_df, space_data)
         possible_toilets_data = _extractor.get_toilets_data(_processor.rm_category_type_df, space_data)
-        # feedback.pushInfo(str(len(possible_meeting_rooms_data)))
-        # feedback.pushInfo('campus code'+campus_code)
-        # feedback.pushInfo(str(possible_meeting_rooms_data['Campus Code']))
-
         filtered_meeting_rooms_data = possible_meeting_rooms_data[possible_meeting_rooms_data['Campus Code'] == campus_code]
         filtered_employee_data = employee_data[employee_data['Campus Code']==campus_code]
         feedback.pushInfo(str(len(filtered_meeting_rooms_data)))
         feedback.pushInfo(str(len(filtered_employee_data)))
-        feedback.pushInfo('# merging data for getting supply')
+        feedback.pushInfo('Merging data for getting supply')
         mr_data = filtered_meeting_rooms_data.groupby(by=['Campus Code','Building Code','Building Name'], as_index=False).agg({'Room Code':pd.Series.nunique,'Room Capacity':sum})
         mr_data = mr_data.rename(columns={"Room Code": "MR_COUNT", "Room Capacity": "MR_CAP"})
-        feedback.pushInfo('# merging data for getting demand')
-
+        feedback.pushInfo('Merging data for getting demand')
         emp_data = filtered_employee_data.groupby(by=['Campus Code','Building Code','Building Name'], as_index=False).agg({'Employee Sequential ID':pd.Series.nunique})
         emp_data = emp_data.rename(columns={'Employee Sequential ID':'EMP_COUNT'})
         feedback.pushInfo(str(len(emp_data)))
-        layer = QgsVectorLayer('C:/Users/deshp/Google Drive/project-space-optimisation-group-3/spatial-qgis/BL_OUTLINE/PAR_BUILDING_OUTLINE/PAR_BUILDING_OUTLINE.shp')
-
-        layer_provider = layer.dataProvider()
-        layer.startEditing()
-        feedback.pushInfo('Started editing'+str(layer))
-
-        feedback.pushInfo(str(layer_provider))
-        weightFieldIndex = layer_provider.fieldNameIndex('MR_WEIGHTS')
-        if weightFieldIndex != -1:
-            layer_provider.deleteAttributes([weightFieldIndex])
-            layer.updateFields()
-
-        layer_provider.addAttributes([QgsField("MR_WEIGHTS",  QVariant.Double)])
-
-        layer.updateFields()
-        weightFieldIndex = layer_provider.fieldNameIndex('MR_WEIGHTS')
-        feedback.pushInfo('weightFieldIndex added'+ str(weightFieldIndex))
-
-        # If sink was not created, throw an exception to indicate that the algorithm
-        # encountered a fatal error. The exception text can be any string, but in this
-        # case we use the pre-built invalidSinkError method to return a standard
-        # helper text for when a sink cannot be evaluated
+        if source is None:
+            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+        (sink, dest_id) = self.parameterAsSink(
+            parameters,
+            self.OUTPUT,
+            context,
+            source.fields(),
+            source.wkbType(),
+            source.sourceCrs()
+        )
+        feedback.pushInfo('CRS is {}'.format(source.sourceCrs().authid()))
         if sink is None:
             raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
-
-        # # Compute the number of steps to display within the progress bar and
-        # # get features from source
-        total = 100.0 / layer.featureCount() if layer.featureCount() else 0
-        features = layer.getFeatures()
-        # layer.startEditing()
-        
+        total = 100.0 / source.featureCount() if source.featureCount() else 0
+        features = source.getFeatures()
         _features = DataFeatures(feedback)
-        for i,feature in enumerate(features):
-            # new_feature =  QgsFeature()
-            # new_feature.setGeometry(feature.geometry())
-            aid = layer_provider.fieldNameIndex('MR_WEIGHTS')
-            
+
+        for current, feature in enumerate(features):
             if feedback.isCanceled():
                 break
+            aid = layer_provider.fieldNameIndex('MR_WEIGHTS')
             id = feature.id()
             supply = _features.get_meeting_room_capacity(mr_data, feature[search_key])
             demand = _features.get_employee_count(emp_data, feature[search_key])
-
             if supply and demand:
                 weight = float(supply/demand)
-                # feedback.pushInfo('weight---'+str(weight))
             else:
                 weight = 'NULL'
-                # feedback.pushInfo('weight---'+str(weight))
-
-            attr_value={aid:weight}
+            feature['MR_WEIGHTS'] = weight
+            attr_value={weightFieldIndex:weight}
             layer_provider.changeAttributeValues({id:attr_value})
-            layer.updateFeature(feature)
-            feedback.setProgress(int(i * total))
-
+            sink.addFeature(feature, QgsFeatureSink.FastInsert)
+            feedback.setProgress(int(current * total))
         layer.commitChanges()
-        return {self.OUTPUT: layer}
+
+        if False:
+            buffered_layer = processing.run("native:buffer", {
+                'INPUT': dest_id,
+                'DISTANCE': 1.5,
+                'SEGMENTS': 5,
+                'END_CAP_STYLE': 0,
+                'JOIN_STYLE': 0,
+                'MITER_LIMIT': 2,
+                'DISSOLVE': False,
+                'OUTPUT': 'memory:'
+            }, context=context, feedback=feedback)['OUTPUT']
+
+        return {self.OUTPUT: dest_id}
+
 
 class DataProcessor():
     def __init__(self,uom_space_url,rm_category_type_url,
@@ -337,7 +233,6 @@ class DataProcessor():
         self.meeting_room_usage_df = pd.read_excel(self.meeting_room_usage_url)
         
         self.feedback.pushInfo('Data loaded successfully!')
-        # output data shapes on the console
         self.feedback.pushInfo("UOM space shape:"+str(self.uom_space_df.shape))
         self.feedback.pushInfo("RM category:"+str(self.rm_category_type_df.shape))
         self.feedback.pushInfo("EM location:"+str(self.em_location_df.shape))
@@ -421,19 +316,10 @@ class DataCleaner():
         return av_equipment_df
 
     def clean_timetable_data(self,timetable_df):
-        # remove all NaN rows
         timetable_df = timetable_df.dropna(how='all')
-
-        # drop duplicate records
         timetable_df = timetable_df.drop_duplicates()
-
-        # dropping classes whose location is not planned
         timetable_df = timetable_df[timetable_df['Host Key of Allocated Locations'].notna()]
-
-        # dropping classes whose location is online option
         timetable_df = timetable_df[timetable_df['Host Key of Allocated Locations']!='Online option.']
-
-        # dropping classes with off-site location
         timetable_df = timetable_df[timetable_df['Name of Zone of Allocated Locations']!='Off-Site']
         return timetable_df
 
@@ -456,7 +342,6 @@ class DataMutator():
         self.feedback = feedback
 
     def mutate_em_location(self,em_location_df):
-        # mutate room codes
         for idx,row in em_location_df.iterrows():
             if "." in row['Room Code']:
                 code = row['Room Code'].split(".")[0]
@@ -493,28 +378,28 @@ class DataMerger():
 
     def get_merged_space_data(self,uom_space_df,rm_category_type_df, floor_df):
         uom_space_df_enhanced = pd.merge(uom_space_df,floor_df,on=['Building Code','Floor Code'])
-        self.feedback.pushInfo("# Merge - uom_space + floor_data")
+        self.feedback.pushInfo("Merge - uom_space + floor_data")
         merged_space_data_df = pd.merge(uom_space_df_enhanced,rm_category_type_df,on=['Room Category','Room Type'])
-        self.feedback.pushInfo("# Merge - enhanced_uom_space + rm_category_type")
+        self.feedback.pushInfo("Merge - enhanced_uom_space + rm_category_type")
         return merged_space_data_df
 
     def get_merged_em_location_data(self,em_location_df,merged_space_data_df):
-        self.feedback.pushInfo("# merge - space_data + em_location")
+        self.feedback.pushInfo("Merge - space_data + em_location")
         merged_em_location_df = pd.merge(em_location_df,merged_space_data_df,on=['Building Code','Floor Code','Room Code'])
         return merged_em_location_df
 
     def get_merged_av_equipment_data(self,av_equipment_df,merged_space_data_df):
-        self.feedback.pushInfo("# merge - space_data + av_equipment")
+        self.feedback.pushInfo("Merge - space_data + av_equipment")
         merged_av_equipment_df = pd.merge(av_equipment_df,merged_space_data_df,on=['Campus Code','Building Code','Floor Code','Room Code'])
         return merged_av_equipment_df
 
     def get_merged_timetable_data(self,timetable_df,merged_space_data_df):
-        self.feedback.pushInfo("# merge - space_data + timetable_data")
+        self.feedback.pushInfo("Merge - space_data + timetable_data")
         merged_timetable_df = pd.merge(timetable_df,merged_space_data_df,on=['Campus Code','Building Code','Room Code'])
         return merged_timetable_df
 
     def get_merged_meeting_room_usage_data(self,meeting_room_usage_df,merged_space_data_df):
-        self.feedback.pushInfo("# merge - space_data + meeting_room_usage")
+        self.feedback.pushInfo("Merge - space_data + meeting_room_usage")
         merged_meeting_room_usage_df = pd.merge(meeting_room_usage_df,merged_space_data_df,on=['Campus Code','Building Code','Floor Code','Room Code'])
         return merged_meeting_room_usage_df
 
@@ -523,11 +408,9 @@ class DataExtractor():
         self.feedback = feedback
 
     def get_meeting_rooms_data(self,rm_category_type_df,space_data):
-        # possible meeting rooms
         possible_rooms = rm_category_type_df[rm_category_type_df['Room Type'].str.contains("601|629")]
         meeting_room_types = possible_rooms['Room Type'].tolist()
 
-        # supply of meeting rooms
         possible_meeting_rooms_df = space_data[space_data['Room Type'].isin(meeting_room_types)]
         return possible_meeting_rooms_df
 
